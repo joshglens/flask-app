f3dcd5a6a113c960c7515c8810abbd542a3897c6
Joshua Goldshteyn <goldshteynj@msoe.edu>
2024-01-31 17:50:51 -0600

    Initial flask-app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def cool_button():
+    cool_button = '''
+        <button class="pushable" style="background: hsl(340deg 100% 32%); border: none; border-radius: 12px; padding: 0; cursor: pointer;">
+            <span class="front" style="display: block; padding: 12px 42px; border-radius: 12px; font-size: 1.25rem; background: hsl(345deg 100% 47%); color: white; transform: translateY(-4px);">
+                Push me
+            </span>
+        </button>\n'''
+    return '<p>Hello Cool Button</p>' + cool_button
+
+
+if __name__ == '__main__':
+    app.run()
